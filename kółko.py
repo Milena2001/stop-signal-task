6f9b4367f8cb8167ee3deb55b1987d8a2beaca0d
Milena2001 <80248630+Milena2001@users.noreply.github.com>
2021-06-29 15:49:37 +0200

    Delete kółko.py

--- kółko.py
+++
@@ -1,8 +0,0 @@
-from psychopy import visual, core, event
-
-window = visual.Window(units="pix", color="gray", fullscr=True)
-circle= visual.Circle(window, size=(120, 120), pos=(0,0), lineColor = 'white', fillColor = None)
-window.flip()
-circle.draw()
-window.flip()
-core.wait(1)
